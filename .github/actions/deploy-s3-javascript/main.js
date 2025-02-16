072d25f1c79e3a0a89fe2764b9ed01a7380bb585
choonsiong <choonsiong.lee@gmail.com>
2025-02-16 15:35:53 +0800

    Auto commit

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,6 +4,15 @@ const exec = require('@actions/exec');
 
 
 function run() {
+    // 1 - get some input values
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // 2 - upload files
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri}`)
+    
     core.notice('Hello from my custom JavaScript action!');
 }
 
