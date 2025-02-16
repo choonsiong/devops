1e812a3228f882d010ed97a973d1dec7e9511998
choonsiong <choonsiong.lee@gmail.com>
2025-02-16 15:40:12 +0800

    Auto commit

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -11,7 +11,7 @@ function run() {
 
     // 2 - upload files
     const s3Uri = `s3://${bucket}`
-    exec.exec(`aws s3 sync ${distFolder} ${s3Uri}`)
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
     
     core.notice('Hello from my custom JavaScript action!');
 }
