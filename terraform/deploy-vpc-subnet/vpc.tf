eaa4a6cf6d6fb94aec5e222c6d30148ddd51ad8e
choonsiong <choonsiong.lee@gmail.com>
2025-03-06 16:46:41 +0800

    Auto commit

--- terraform/deploy-vpc-subnet/vpc.tf
+++ terraform/deploy-vpc-subnet/vpc.tf
@@ -12,15 +12,28 @@ resource "aws_vpc" "terraform_demo_vpc" {
 }
 
 resource "aws_subnet" "terraform_public_subnet" {
-    vpc_id = aws_vpc.terraform_demo_vpc
+    vpc_id = aws_vpc.terraform_demo_vpc.id
     cidr_block = "10.1.0.0/24"
 }
 
 resource "aws_subnet" "terraform_private_subnet" {
-    vpc_id = aws_vpc.terraform_demo_vpc
+    vpc_id = aws_vpc.terraform_demo_vpc.id
     cidr_block = "10.1.1.0/24"
 }
 
 resource "aws_internet_gateway" "terraform_igw" {
-    vpc_id = aws_vpc.terraform_demo_vpc
+    vpc_id = aws_vpc.terraform_demo_vpc.id
+}
+
+resource "aws_route_table" "terraform_public_rtb" {
+    vpc_id = aws_vpc.terraform_demo_vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.terraform_igw.id
+    }
+}
+
+resource "aws_route_table_association" "terraform_public_subnet_1" {
+    subnet_id = aws_subnet.terraform_public_subnet.id
+    route_table_id = aws_route_table.terraform_public_rtb.id
 }
