1dfcf9ba6aae19576f1acaa80ab07576f8400bd7
choonsiong <choonsiong.lee@gmail.com>
2025-05-07 22:34:48 +0800

    Auto commit

--- terraform/3-terraform-block/block.tf
+++ terraform/3-terraform-block/block.tf
@@ -1,5 +1,6 @@
 terraform {
     required_version = "1.7.0"
+    //required_version = ">1.5.0, <1.7.0"
 
     backend "s3" {
         // ...
@@ -8,7 +9,7 @@ terraform {
     required_providers {
         aws = {
             source  = "hashicorp/aws"
-            version = "~> 5.33.0"
+            version = "~> 5.33.0" // allows only the rightmost digit to increment (e.g., the minor version here is allowed to increment)
         }
     }
 }
