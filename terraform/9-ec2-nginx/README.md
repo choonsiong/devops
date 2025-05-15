12d00235424c3ffce0877dc39336fd163023f8b8
choonsiong <choonsiong.lee@gmail.com>
2025-05-15 21:13:45 +0800

    Auto commit

--- terraform/9-ec2-nginx/README.md
+++ terraform/9-ec2-nginx/README.md
@@ -3,7 +3,7 @@ Steps for implementation:
 1. Deploy a VPC and a subnet
 2. Deploy an Internet Gateway and associate it with the VPC
 3. Setup a route table with a route to the IGW
-4. Deploy an EC2 instance inside of the created subnet
-5. Associate a public IP and a security group that allows public ingress
+4. Deploy an EC2 instance inside of the created subnet and associate a public IP
+5. Associate a security group that allows public ingress
 6. Change the EC2 instance to use a publicly available Nginx AMI
 7. Destroy everything
--- terraform/9-ec2-nginx/compute.tf
+++ terraform/9-ec2-nginx/compute.tf
@@ -1,7 +1,29 @@
-resource "aws_instance" "web" {
+`resource "aws_instance" "web" {
   # https://cloud-images.ubuntu.com/locator/ec2/
-  ami = "ami-0892da582b5039419"
+  ami                         = "ami-0892da582b5039419"
   associate_public_ip_address = true
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.public.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public.id
+}
+
+resource "aws_security_group" "public_http_traffic" {
+  description = "Security group allowing traffic on ports 443 and 80"
+  name        = "public-http-traffic-sg"
+  vpc_id      = aws_vpc.main.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http_ingress" {
+  security_group_id = aws_security_group.public_http_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https_ingress" {
+  security_group_id = aws_security_group.public_http_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
 }
