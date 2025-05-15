cf06031a41f119dd95f55fb8482b34df156c477f
choonsiong <choonsiong.lee@gmail.com>
2025-05-15 18:51:40 +0800

    Auto commit

--- terraform/9-ec2-nginx/networking.tf
+++ terraform/9-ec2-nginx/networking.tf
@@ -18,3 +18,27 @@ resource "aws_subnet" "public" {
     Project   = "9-ec2-nginx"
   }
 }
+
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name      = "9-ec2-nginx-igw"
+    ManagedBy = "Terraform"
+    Project   = "9-ec2-nginx"
+  }
+}
+
+resource "aws_route_table" "main_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.main_rt.id
+}
