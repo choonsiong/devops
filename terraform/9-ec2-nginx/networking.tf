a8dcfdf738f669a92127d53d4b401720295cd0b1
choonsiong <choonsiong.lee@gmail.com>
2025-05-15 19:28:19 +0800

    Auto commit

--- terraform/9-ec2-nginx/networking.tf
+++ terraform/9-ec2-nginx/networking.tf
@@ -1,32 +1,33 @@
+locals {
+  common_tags = {
+    ManagedBy  = "Terraform"
+    Project    = "9-ec2-nginx"
+    CostCenter = "1234"
+  }
+}
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
-  tags = {
-    Name      = "9-ec2-nginx"
-    ManagedBy = "Terraform"
-    Project   = "9-ec2-nginx"
-  }
+  tags = merge(local.common_tags, {
+    Name = "9-ec2-nginx"
+  })
 }
 
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.0/24"
 
-  tags = {
-    Name      = "9-ec2-nginx-public"
-    ManagedBy = "Terraform"
-    Project   = "9-ec2-nginx"
-  }
+  tags = merge(local.common_tags, {
+    Name = "9-ec2-nginx-public"
+  })
 }
 
 resource "aws_internet_gateway" "main_igw" {
   vpc_id = aws_vpc.main.id
 
-  tags = {
-    Name      = "9-ec2-nginx-igw"
-    ManagedBy = "Terraform"
-    Project   = "9-ec2-nginx"
-  }
+  tags = merge(local.common_tags, {
+    Name = "9-ec2-nginx-main-igw"
+  })
 }
 
 resource "aws_route_table" "main_rt" {
@@ -36,9 +37,13 @@ resource "aws_route_table" "main_rt" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main_igw.id
   }
+
+  tags = merge(local.common_tags, {
+    Name = "9-ec2-nginx-main-route-table"
+  })
 }
 
 resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.main_rt.id
-}
+`}
