c5b27e87e2380083ae179f621877a3389026a414
choonsiong <choonsiong.lee@gmail.com>
2025-05-21 16:13:40 +0800

    Auto commit

--- terraform/14-data-source-iam-policy/compute.tf
+++ terraform/14-data-source-iam-policy/compute.tf
@@ -14,6 +14,20 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+data "aws_iam_policy_document" "static_website" {
+  statement {
+    sid = "PublicReadGetObject"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions = ["s3:GetObject"]
+    resources = [
+      "${aws_s3_bucket.public_read_bucket.arn}/*"
+    ]
+  }
+}
+
 # This vpc is managed manually on AWS console, we just 'use it' here
 data "aws_vpc" "prod_vpc" {
   tags = {
@@ -29,6 +43,10 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {
   #provider = aws.ap-southeast-2
+}
+
+output "iam_policy" {
+  value = data.aws_iam_policy_document.static_website
 }
 
 output "prod_vpc_id" {
@@ -55,4 +73,8 @@ resource "aws_instance" "web" {
   ami                         = data.aws_ami.ubuntu.id
   associate_public_ip_address = true
   instance_type               = "t2.micro"
+}
+
+resource "aws_s3_bucket" "public_read_bucket" {
+  bucket = "lcs-data-source-demo-bucket"
 }
