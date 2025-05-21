3b8650ee14d81fd28d3df4c0780f8008f38fe19d
choonsiong <choonsiong.lee@gmail.com>
2025-05-21 15:43:32 +0800

    Auto commit

---
+++ terraform/12-data-source-caller-id/compute.tf
@@ -0,0 +1,37 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+output "ubuntu-ami-test" {
+  value = data.aws_ami.ubuntu.id
+}
+
+output "aws_caller_identity" {
+  value = data.aws_caller_identity.current
+}
+
+output "aws_region" {
+  value = data.aws_region.current
+}
+
+resource "aws_instance" "web" {
+  ami                         = data.aws_ami.ubuntu.id
+  associate_public_ip_address = true
+  instance_type               = "t2.micro"
+}
