fd76b4710f5ba9c3e940db9730bf8c63c869517e
choonsiong <choonsiong.lee@gmail.com>
2025-05-21 16:03:01 +0800

    Auto commit

---
+++ terraform/14-data-source-iam-policy/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.7.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+provider "aws" {
+  alias  = "ap-southeast-2"
+  region = "ap-southeast-2"
+}
