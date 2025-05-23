115bae8186ebe97e19e5aedc56a93ec239a9a4b8
choonsiong <choonsiong.lee@gmail.com>
2025-05-23 17:54:03 +0800

    Auto commit

--- terraform/15-input-vars/variables.tf
+++ terraform/15-input-vars/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
-  type = string
-  #   default     = "ap-southeast-1"
+  type        = string
+  default     = "ap-southeast-1"
   description = "The AWS region to deploy the resources in."
 }   
