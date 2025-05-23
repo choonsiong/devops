cf384e5bcd08423aaa4d55ddd5ff6e3fec13fb60
choonsiong <choonsiong.lee@gmail.com>
2025-05-23 18:10:46 +0800

    Auto commit

--- terraform/15-input-variables/compute.tf
+++ terraform/15-input-variables/compute.tf
@@ -16,5 +16,11 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "compute" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = var.ec2_instance_type
+
+  root_block_device {
+    delete_on_termination = true
+    volume_type           = var.ec2_volume_type
+    volume_size           = var.ec2_volume_size
+  }
 }
--- terraform/15-input-variables/variables.tf
+++ terraform/15-input-variables/variables.tf
@@ -2,4 +2,20 @@ variable "aws_region" {
   type        = string
   default     = "ap-southeast-1"
   description = "The AWS region to deploy the resources in."
-}   
+}
+
+variable "ec2_instance_type" {
+  type        = string
+  default     = "t2.micro"
+  description = "The type of the EC2 instance."
+}
+
+variable "ec2_volume_type" {
+  type        = string
+  description = "The volume type between gp2 and gp3."
+}
+
+variable "ec2_volume_size" {
+  type        = number
+  description = "The size in GB of the root block volume attached to the instance."
+}
