406cbb5513d6108d560bd3c342e83a667821688c
choonsiong <choonsiong.lee@gmail.com>
2025-05-23 18:12:19 +0800

    Auto commit

--- terraform/15-input-variables/variables.tf
+++ terraform/15-input-variables/variables.tf
@@ -8,6 +8,11 @@ variable "ec2_instance_type" {
   type        = string
   default     = "t2.micro"
   description = "The type of the EC2 instance."
+
+  validation {
+    condition = var.ec2_instance_type == "t2.micro" || var.ec2_instance_type == "t3.micro"
+    error_message = "Only supports t2.micro and t3.micro."
+  }
 }
 
 variable "ec2_volume_type" {
