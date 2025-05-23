c85c0652d4485d748aa949aabfc872bf0b34c2ab
choonsiong <choonsiong.lee@gmail.com>
2025-05-23 18:49:50 +0800

    Auto commit

--- terraform/16-tfvars-files/variables.tf
+++ terraform/16-tfvars-files/variables.tf
@@ -10,21 +10,10 @@ variable "ec2_instance_type" {
   description = "The type of the EC2 instance."
 
   validation {
-    #condition     = var.ec2_instance_type == "t2.micro" || var.ec2_instance_type == "t3.micro"
     condition     = contains(["t2.micro", "t3.micro"], var.ec2_instance_type)
     error_message = "Only supports t2.micro and t3.micro."
   }
 }
-
-#variable "ec2_volume_type" {
-#  type        = string
-#  description = "The volume type between gp2 and gp3."
-#}
-#
-#variable "ec2_volume_size" {
-#  type        = number
-#  description = "The size in GB of the root block volume attached to the instance."
-#}
 
 variable "ec2_volume_config" {
   type = object({
